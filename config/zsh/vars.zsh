f7e271c925e803e341e7947dca3dda0e5885257c
Michael Shustin <michaelshustin@gmail.com>
2025-01-12 17:56:37 +0200

    Removed bat theme

--- config/zsh/vars.zsh
+++ config/zsh/vars.zsh
@@ -3,9 +3,6 @@
 # You can always avoid the aliased command by running "\cat" instead of "cat", etc.
 export DROP_IN_REPLACEMENTS=1
 
-# Bat theme
-export BAT_THEME=default
-
 # Eza params
 export EZA_PARAMS="--octal-permissions --no-permissions --icons --header"
 
