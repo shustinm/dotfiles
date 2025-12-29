a03c6e5658064bcd333e64af1d97cb4b4c2e32b7
Michael Shustin <michaelshustin@gmail.com>
2025-12-29 11:42:09 +0200

    lazyvim

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.lazyvim_python_lsp = "basedpyright"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.g.autoformat = false
