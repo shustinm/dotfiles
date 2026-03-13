2c4974f26929f700cefe1dc6ae9e7762c481875f
Michael Shustin <michaelshustin@gmail.com>
2026-03-13 17:04:03 +0200

    nvim clipboard over ssh

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -4,6 +4,22 @@
 
 vim.g.lazyvim_python_lsp = "basedpyright"
 
+-- Force OSC 52 clipboard over SSH so yanks reach the local clipboard
+if vim.env.SSH_TTY then
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+end
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
